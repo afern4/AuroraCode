0e58ddf4bf442dc9b6a8f6afb953af230cd74f56
afern4 <149612378+afern4@users.noreply.github.com>
2023-11-01 13:22:37 -0400

    Update and rename Aurora Code to AuroraCode.ino

--- AuroraCode.ino
+++ AuroraCode.ino
@@ -1,29 +1,30 @@
-//Project 1 for instrumnetation Lab  
+//ARDUINO INSTRUMENTATION LAB
+//Project 1 for instrumentation Lab  
 // Savanna, Beverly & Alexis  
 // 25th October, 2023 
 #include <NewPing.h>                // Used as an interface with the ultrasonic sensor, which helps us measure the distance to obstacles 
  
  
 #define TRIGGER_PIN 6               // Choosing a trigger pin 
-#define ECHO_PIN 7                  // Stating the pin on arduino that will be connected to echo sensor 
+#define ECHO_PIN 7                  // Stating the pin on Arduino that will be connected to the echo sensor 
 #define MAX_DISTANCE 200           // Stating the maximum distance between Aurora and Obstacle   
  
  
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
  
  
-#define motor1Pin1 2              // Stating the pin on arduino that will be connected to right back wheel 
-#define motor1Pin2 3              // Stating the pin on arduino that will be connected to right front wheel 
-#define motor2Pin1 4              // Stating the pin on arduino that will be connected to left front wheel 
-#define motor2Pin2 5              // Stating the pin on arduino that will be connected to left back wheel 
+#define motor1Pin1 2              // Stating the pin on Arduino that will be connected to the right back wheel 
+#define motor1Pin2 3              // Stating the pin on Arduino that will be connected to the right front wheel 
+#define motor2Pin1 4              // Stating the pin on Arduino that will be connected to the left front wheel 
+#define motor2Pin2 5              // Stating the pin on Arduino that will be connected to the left back wheel 
  
  
 void setup() { 
   // Initialize motor control pins as outputs 
-  pinMode(motor1Pin1, OUTPUT);       // Setting pin for right back wheel as an output pin 
-  pinMode(motor1Pin2, OUTPUT);       // Setting pin for right front wheel as an output pin 
-  pinMode(motor2Pin1, OUTPUT);       // Setting pin for left front wheel as an output pin 
-  pinMode(motor2Pin2, OUTPUT);       // Setting pin for left back wheel as an output pin 
+  pinMode(motor1Pin1, OUTPUT);       // Setting pin for the right back wheel as an output pin 
+  pinMode(motor1Pin2, OUTPUT);       // Setting pin for the right front wheel as an output pin 
+  pinMode(motor2Pin1, OUTPUT);       // Setting pin for the left front wheel as an output pin 
+  pinMode(motor2Pin2, OUTPUT);       // Setting pin for the left back wheel as an output pin 
 } 
  
  
